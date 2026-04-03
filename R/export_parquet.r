b0d553060cb6c1aaf1878aefbf6b39f60e037028
Anishkumar Senthil <anishksenthil@gmail.com>
2026-04-03 12:07:52 -0700

    Parquet Exporting and new parquet files

---
+++ R/export_parquet.r
@@ -0,0 +1,21 @@
+source("R/db_init.R")
+out_dir <- "R/parquet"
+
+if(!dir.exists(out_dir)){
+    dir.create(out_dir, recursive = TRUE)
+}
+
+tables <- DBI::dbListTables(con)
+message("Tables being exported:")
+print(tables)
+
+for (tbl in tables) {
+    out_file <- file.path(out_dir,paste0(tbl, ".parquet"))
+
+    sql <- paste0("COPY ", DBI::dbQuoteIdentifier(con,tbl), " To '", out_file, "' (FORMAT PARQUET, COMPRESSION ZSTD);")
+
+    message("Exporting ", tbl, " to ", out_file)
+    DBI::dbExecute(con,sql)
+}
+
+message("Parquet Export done")
